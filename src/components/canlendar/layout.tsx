e3215790af72f3a93ea67e684c58de3b4b1de48b
p_rxzhou <p_rxzhou@tencent.com>
2021-01-20 10:15:04 +0800

    scss suport etc

---
+++ src/components/canlendar/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { CanlendarLayoutProps } from './index'
+import s from './layout.module.scss'
+
+export function CanlendarLayout(props: CanlendarLayoutProps) {
+    return (
+        <div className={s.layout}>
+            layout
+        </div>
+    )
+}
---
+++ src/pages/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CanlendarLayout } from '../components/canlendar/layout'
+
+export default function Main() {
+    return (
+        <div>main
+            <CanlendarLayout />
+        </div>
+    )
+}
