76cec749628c21562afea4125de116d34ae8d333
p_rxzhou <p_rxzhou@tencent.com>
2021-01-22 10:47:35 +0800

    web notification test

--- src/components/canlendar/layout.tsx
+++ src/components/canlendar/layout.tsx
@@ -41,47 +41,68 @@ export function CanlendarLayout(props: CanlendarLayoutProps) {
     const { settings } = props
     const { firstDayToShow } = settings
 
+    // states
     // 星期
     let weeks = `日一二三四五六`.split('')
     weeks = weeks.concat(weeks.splice(0, firstDayToShow))
 
     // 这个月
     const [currentDate, setCurrentDate] = useState(dayjs(dayjs().startOf("date")))
+    const [baseDate, setBaseDate] = useState(currentDate)
+    const [units, setUnits] = useState<any>([])
 
-    const firstDate = currentDate.startOf("month")
-    const lastDate = currentDate.endOf('month')
+    // effects
+    useEffect(() => {
+        const firstDate = baseDate.startOf("month")
+        const lastDate = baseDate.endOf('month')
+        const indexOfFirstDate = firstDate.get('day')
+        const indexOfLastDate = lastDate.get('day')
+        const tunits = []
+        let headUnit = dayjs(firstDate).add(-indexOfFirstDate + firstDayToShow, 'day')
+        let tailUnit = dayjs(lastDate).add(6 - indexOfLastDate + firstDayToShow, 'day')
+        while (headUnit < tailUnit) {
+            tunits.push({
+                date: headUnit,
+                isToday: headUnit.isSame(currentDate),
+                isInMonth: headUnit.isSame(baseDate, "month")
+            })
+            headUnit = headUnit.add(1, 'day')
+        }
+        setUnits(tunits)
+    }, [baseDate])
 
+    // memos
+    const baseDateFormat = useMemo(() => baseDate.format('YYYY-MM'), [baseDate])
 
-    const indexOfFirstDate = firstDate.get('day')
-    const indexOfLastDate = lastDate.get('day')
+    // functions
+    function handleGoPrevMonth() {
+        setBaseDate(baseDate.add(-1, 'month').startOf('date'))
 
-    const units = []
-    let headUnit = dayjs(firstDate).add(-indexOfFirstDate + firstDayToShow, 'day')
-    let tailUnit = dayjs(lastDate).add(6 - indexOfLastDate + firstDayToShow, 'day')
-
-    // console.log(currentDate.toDate().toLocaleDateString())
-    // console.log(headUnit.toDate().toLocaleDateString())
-    // console.log(tailUnit.toDate().toLocaleDateString())
-
-    while (headUnit < tailUnit) {
-        units.push({
-            date: headUnit,
-            isToday: headUnit.isSame(currentDate),
-            isInMonth: headUnit.isSame(currentDate, "month")
-        })
-        headUnit = headUnit.add(1, 'day')
+        // Notification.requestPermission()
+        // Notification.permission
+        Notification.requestPermission().then(function (result) {
+            // result可能是是granted, denied, 或default.
+            console.log(result)
+            var n = new Notification("title", {
+                body: "notification body"
+            }); // 显示通知
+            n.onshow = function () {
+                setTimeout(n.close.bind(n), 5000);
+            }
+        });
     }
-
-    const currentDateFormat = useMemo(() => currentDate.format('YYYY-MM-DD'), [currentDate])
+    function handleGoNextMonth() {
+        setBaseDate(baseDate.add(1, 'month').startOf('date'))
+    }
 
     return (
         <div className={s.layout}>
             <div className={s['title']}>
-                <div className={s["l"]}>{currentDateFormat}</div>
+                <div className={s["l"]}>{baseDateFormat}</div>
                 <div className={s["c"]}></div>
                 <div className={s["r"]}>
-                    <div className={s["prev"]} />
-                    <div className={s["next"]} />
+                    <div className={s["prev"]} onClick={handleGoPrevMonth} />
+                    <div className={s["next"]} onClick={handleGoNextMonth} />
                 </div>
             </div>
             <div className={s["days"]}>
