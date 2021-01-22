8d2212ef441736c4e5b9b7bd3680a57b64f104b5
SamYao <yaob@miamioh.edu>
2021-01-22 14:33:29 +0800

    add

---
+++ src/components/navigation/index.tsx
@@ -0,0 +1,30 @@
+import React, { Fragment } from "react";
+import s from "./style.module.scss";
+import { ImageIcons } from "../../utils/dom-assets";
+import { INavigationProps, ModuleOption } from "./defined";
+
+const Navigation = (props: any) => {
+  const { moduleOptions } = props;
+    console.log('props', props)
+  return (
+    <Fragment>
+      {props.children}
+      <div className={s.nav_container}>
+        {moduleOptions.map((el: ModuleOption) => (
+          <div key={el.name} className={s.nav_item}>
+            <ImageIcons
+              type={el.icon}
+              size={32}
+              onClick={() => {
+                props.history.push(el.route);
+              }}
+            />
+            <span>{el.name}</span>
+          </div>
+        ))}
+      </div>
+    </Fragment>
+  );
+};
+
+export default Navigation;
