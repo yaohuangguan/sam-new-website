d3177eeb7c731fd9308ce003c843509bf494e814
p_rxzhou <p_rxzhou@tencent.com>
2021-04-25 16:51:38 +0800

    fix docker file

--- serve/base.sh
+++ serve/base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # step1 打基础镜像
-images_name="bat-assets"
+images_name="yr-canlendar"
 
-docker build -t $images_name:base -f ./serve/base.Dockerfile .
+docker build -t $images_name:base -f ./serve/Dockerfile .
